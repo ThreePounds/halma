196e786c2070076196508b1de2cec19084b503c8
mail@tobiashuth.de <mail@tobiashuth.de>
2023-04-17 22:46:43 +0200

    implementing iterator logic

--- src/halma/model.py
+++ src/halma/model.py
@@ -1,44 +1,60 @@
 from dataclasses import dataclass
 from enum import IntEnum, auto
 from functools import cached_property
-from typing import Iterator
 
 class Content(IntEnum):
     NONE = 0
     EMPTY = auto()
     PEG = auto()
 
-@dataclass(frozen=True)
-class Tile():
-    index: int
-    row: int
-    column: int
-    content: Content = Content.NONE
+Directions = {
+    'up': -10,
+    'right': 1,
+    'down': 10,
+    'left': -1,
+}
 
 @dataclass(frozen=True)
 class Board():
-    tiles: tuple[Tile, ...]
-    
-    def __iter__(self) -> Iterator[Tile]:
-        return iter(self.tiles)
-    
-    def __getitem__(self, index: int) -> Tile:
+    tiles: dict[int, Content]
+
+    def __iter__(self):
+        return iter(self.tiles.items())
+
+    def __getitem__(self, index: int):
         return self.tiles[index]
-   
-    @classmethod
-    def English(cls):
-        """New empty English board."""
-        board = []
-        for x in range(7):
-            for y in range(7):
-                if x in [0, 1, 5, 6] and y in [0, 1, 5, 6]:
-                    # corner
-                    board.append(Tile(x * 10 + y, x, y))
-                else:
-                    # empty tile
-                    board.append(Tile(x * 10 + y, x, y, Content.EMPTY))
-        return cls(tuple(board))
 
     @cached_property
-    def pegs(self) -> Tile:
-        return iter(tile for tile in self if tile.content is Content.PEG)
+    def moves(self):
+        for index in self.pegs:
+            for offset in Directions.values():
+                try:
+                    if (
+                        self[index + offset] is Content.PEG 
+                        and self[index + offset * 2] is Content.EMPTY
+                    ):
+                        yield (index, offset)
+                except KeyError:
+                    pass
+                    
+    def move_peg(self, index, offset):
+        tiles = self.tiles.copy()
+        tiles[index] = Content.EMPTY
+        tiles[index + offset] = Content.EMPTY
+        tiles[index + offset * 2] = Content.PEG
+        return self.__class__(tiles)
+    
+    @cached_property
+    def pegs(self):
+        return tuple(index for index, content in self if content is Content.PEG)
+    
+    @cached_property
+    def holes(self):
+        return tuple(index for index, content in self if content is Content.EMPTY)
+
+new_board = Board({0: Content.PEG, 1: Content.PEG, 2: Content.EMPTY, 3: Content.PEG, 4: Content.EMPTY})
+
+if __name__ == '__main__':
+    for move in new_board.moves:
+        index, offset = move
+        
